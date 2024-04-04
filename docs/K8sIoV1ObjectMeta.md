6a798d55152bad013bbad2a2c0d8e5a178d04512
Drew Mullen <mullen.drew@gmail.com>
2024-04-04 08:21:23 -0500

    temporarily use a swagger that DOES NOT ignore ObjectMeta

--- docs/K8sIoV1ObjectMeta.md
+++ docs/K8sIoV1ObjectMeta.md
@@ -4,8 +4,21 @@
 
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
+**Annotations** | Pointer to **map[string]string** |  | [optional] 
+**CreationTimestamp** | Pointer to **string** |  | [optional] [default to "{}"]
+**DeletionGracePeriodSeconds** | Pointer to **int64** |  | [optional] 
+**DeletionTimestamp** | Pointer to **string** |  | [optional] [default to ""]
+**Finalizers** | Pointer to **[]string** |  | [optional] 
+**GenerateName** | Pointer to **string** |  | [optional] 
+**Generation** | Pointer to **int64** |  | [optional] 
+**Labels** | Pointer to **map[string]string** |  | [optional] 
+**ManagedFields** | Pointer to [**[]K8sIoV1ManagedFieldsEntry**](K8sIoV1ManagedFieldsEntry.md) |  | [optional] 
 **Name** | **string** |  | 
 **Namespace** | Pointer to **string** |  | [optional] 
+**OwnerReferences** | Pointer to [**[]K8sIoV1OwnerReference**](K8sIoV1OwnerReference.md) |  | [optional] 
+**ResourceVersion** | Pointer to **string** |  | [optional] 
+**SelfLink** | Pointer to **string** |  | [optional] 
+**Uid** | Pointer to **string** |  | [optional] 
 
 ## Methods
 
@@ -25,6 +38,231 @@ will change when the set of required properties is changed
 NewK8sIoV1ObjectMetaWithDefaults instantiates a new K8sIoV1ObjectMeta object
 This constructor will only assign default values to properties that have it defined,
 but it doesn't guarantee that properties required by API are set
+
+### GetAnnotations
+
+`func (o *K8sIoV1ObjectMeta) GetAnnotations() map[string]string`
+
+GetAnnotations returns the Annotations field if non-nil, zero value otherwise.
+
+### GetAnnotationsOk
+
+`func (o *K8sIoV1ObjectMeta) GetAnnotationsOk() (*map[string]string, bool)`
+
+GetAnnotationsOk returns a tuple with the Annotations field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetAnnotations
+
+`func (o *K8sIoV1ObjectMeta) SetAnnotations(v map[string]string)`
+
+SetAnnotations sets Annotations field to given value.
+
+### HasAnnotations
+
+`func (o *K8sIoV1ObjectMeta) HasAnnotations() bool`
+
+HasAnnotations returns a boolean if a field has been set.
+
+### GetCreationTimestamp
+
+`func (o *K8sIoV1ObjectMeta) GetCreationTimestamp() string`
+
+GetCreationTimestamp returns the CreationTimestamp field if non-nil, zero value otherwise.
+
+### GetCreationTimestampOk
+
+`func (o *K8sIoV1ObjectMeta) GetCreationTimestampOk() (*string, bool)`
+
+GetCreationTimestampOk returns a tuple with the CreationTimestamp field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetCreationTimestamp
+
+`func (o *K8sIoV1ObjectMeta) SetCreationTimestamp(v string)`
+
+SetCreationTimestamp sets CreationTimestamp field to given value.
+
+### HasCreationTimestamp
+
+`func (o *K8sIoV1ObjectMeta) HasCreationTimestamp() bool`
+
+HasCreationTimestamp returns a boolean if a field has been set.
+
+### GetDeletionGracePeriodSeconds
+
+`func (o *K8sIoV1ObjectMeta) GetDeletionGracePeriodSeconds() int64`
+
+GetDeletionGracePeriodSeconds returns the DeletionGracePeriodSeconds field if non-nil, zero value otherwise.
+
+### GetDeletionGracePeriodSecondsOk
+
+`func (o *K8sIoV1ObjectMeta) GetDeletionGracePeriodSecondsOk() (*int64, bool)`
+
+GetDeletionGracePeriodSecondsOk returns a tuple with the DeletionGracePeriodSeconds field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetDeletionGracePeriodSeconds
+
+`func (o *K8sIoV1ObjectMeta) SetDeletionGracePeriodSeconds(v int64)`
+
+SetDeletionGracePeriodSeconds sets DeletionGracePeriodSeconds field to given value.
+
+### HasDeletionGracePeriodSeconds
+
+`func (o *K8sIoV1ObjectMeta) HasDeletionGracePeriodSeconds() bool`
+
+HasDeletionGracePeriodSeconds returns a boolean if a field has been set.
+
+### GetDeletionTimestamp
+
+`func (o *K8sIoV1ObjectMeta) GetDeletionTimestamp() string`
+
+GetDeletionTimestamp returns the DeletionTimestamp field if non-nil, zero value otherwise.
+
+### GetDeletionTimestampOk
+
+`func (o *K8sIoV1ObjectMeta) GetDeletionTimestampOk() (*string, bool)`
+
+GetDeletionTimestampOk returns a tuple with the DeletionTimestamp field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetDeletionTimestamp
+
+`func (o *K8sIoV1ObjectMeta) SetDeletionTimestamp(v string)`
+
+SetDeletionTimestamp sets DeletionTimestamp field to given value.
+
+### HasDeletionTimestamp
+
+`func (o *K8sIoV1ObjectMeta) HasDeletionTimestamp() bool`
+
+HasDeletionTimestamp returns a boolean if a field has been set.
+
+### GetFinalizers
+
+`func (o *K8sIoV1ObjectMeta) GetFinalizers() []string`
+
+GetFinalizers returns the Finalizers field if non-nil, zero value otherwise.
+
+### GetFinalizersOk
+
+`func (o *K8sIoV1ObjectMeta) GetFinalizersOk() (*[]string, bool)`
+
+GetFinalizersOk returns a tuple with the Finalizers field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetFinalizers
+
+`func (o *K8sIoV1ObjectMeta) SetFinalizers(v []string)`
+
+SetFinalizers sets Finalizers field to given value.
+
+### HasFinalizers
+
+`func (o *K8sIoV1ObjectMeta) HasFinalizers() bool`
+
+HasFinalizers returns a boolean if a field has been set.
+
+### GetGenerateName
+
+`func (o *K8sIoV1ObjectMeta) GetGenerateName() string`
+
+GetGenerateName returns the GenerateName field if non-nil, zero value otherwise.
+
+### GetGenerateNameOk
+
+`func (o *K8sIoV1ObjectMeta) GetGenerateNameOk() (*string, bool)`
+
+GetGenerateNameOk returns a tuple with the GenerateName field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetGenerateName
+
+`func (o *K8sIoV1ObjectMeta) SetGenerateName(v string)`
+
+SetGenerateName sets GenerateName field to given value.
+
+### HasGenerateName
+
+`func (o *K8sIoV1ObjectMeta) HasGenerateName() bool`
+
+HasGenerateName returns a boolean if a field has been set.
+
+### GetGeneration
+
+`func (o *K8sIoV1ObjectMeta) GetGeneration() int64`
+
+GetGeneration returns the Generation field if non-nil, zero value otherwise.
+
+### GetGenerationOk
+
+`func (o *K8sIoV1ObjectMeta) GetGenerationOk() (*int64, bool)`
+
+GetGenerationOk returns a tuple with the Generation field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetGeneration
+
+`func (o *K8sIoV1ObjectMeta) SetGeneration(v int64)`
+
+SetGeneration sets Generation field to given value.
+
+### HasGeneration
+
+`func (o *K8sIoV1ObjectMeta) HasGeneration() bool`
+
+HasGeneration returns a boolean if a field has been set.
+
+### GetLabels
+
+`func (o *K8sIoV1ObjectMeta) GetLabels() map[string]string`
+
+GetLabels returns the Labels field if non-nil, zero value otherwise.
+
+### GetLabelsOk
+
+`func (o *K8sIoV1ObjectMeta) GetLabelsOk() (*map[string]string, bool)`
+
+GetLabelsOk returns a tuple with the Labels field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetLabels
+
+`func (o *K8sIoV1ObjectMeta) SetLabels(v map[string]string)`
+
+SetLabels sets Labels field to given value.
+
+### HasLabels
+
+`func (o *K8sIoV1ObjectMeta) HasLabels() bool`
+
+HasLabels returns a boolean if a field has been set.
+
+### GetManagedFields
+
+`func (o *K8sIoV1ObjectMeta) GetManagedFields() []K8sIoV1ManagedFieldsEntry`
+
+GetManagedFields returns the ManagedFields field if non-nil, zero value otherwise.
+
+### GetManagedFieldsOk
+
+`func (o *K8sIoV1ObjectMeta) GetManagedFieldsOk() (*[]K8sIoV1ManagedFieldsEntry, bool)`
+
+GetManagedFieldsOk returns a tuple with the ManagedFields field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetManagedFields
+
+`func (o *K8sIoV1ObjectMeta) SetManagedFields(v []K8sIoV1ManagedFieldsEntry)`
+
+SetManagedFields sets ManagedFields field to given value.
+
+### HasManagedFields
+
+`func (o *K8sIoV1ObjectMeta) HasManagedFields() bool`
+
+HasManagedFields returns a boolean if a field has been set.
 
 ### GetName
 
@@ -70,6 +308,106 @@ SetNamespace sets Namespace field to given value.
 `func (o *K8sIoV1ObjectMeta) HasNamespace() bool`
 
 HasNamespace returns a boolean if a field has been set.
+
+### GetOwnerReferences
+
+`func (o *K8sIoV1ObjectMeta) GetOwnerReferences() []K8sIoV1OwnerReference`
+
+GetOwnerReferences returns the OwnerReferences field if non-nil, zero value otherwise.
+
+### GetOwnerReferencesOk
+
+`func (o *K8sIoV1ObjectMeta) GetOwnerReferencesOk() (*[]K8sIoV1OwnerReference, bool)`
+
+GetOwnerReferencesOk returns a tuple with the OwnerReferences field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetOwnerReferences
+
+`func (o *K8sIoV1ObjectMeta) SetOwnerReferences(v []K8sIoV1OwnerReference)`
+
+SetOwnerReferences sets OwnerReferences field to given value.
+
+### HasOwnerReferences
+
+`func (o *K8sIoV1ObjectMeta) HasOwnerReferences() bool`
+
+HasOwnerReferences returns a boolean if a field has been set.
+
+### GetResourceVersion
+
+`func (o *K8sIoV1ObjectMeta) GetResourceVersion() string`
+
+GetResourceVersion returns the ResourceVersion field if non-nil, zero value otherwise.
+
+### GetResourceVersionOk
+
+`func (o *K8sIoV1ObjectMeta) GetResourceVersionOk() (*string, bool)`
+
+GetResourceVersionOk returns a tuple with the ResourceVersion field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetResourceVersion
+
+`func (o *K8sIoV1ObjectMeta) SetResourceVersion(v string)`
+
+SetResourceVersion sets ResourceVersion field to given value.
+
+### HasResourceVersion
+
+`func (o *K8sIoV1ObjectMeta) HasResourceVersion() bool`
+
+HasResourceVersion returns a boolean if a field has been set.
+
+### GetSelfLink
+
+`func (o *K8sIoV1ObjectMeta) GetSelfLink() string`
+
+GetSelfLink returns the SelfLink field if non-nil, zero value otherwise.
+
+### GetSelfLinkOk
+
+`func (o *K8sIoV1ObjectMeta) GetSelfLinkOk() (*string, bool)`
+
+GetSelfLinkOk returns a tuple with the SelfLink field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetSelfLink
+
+`func (o *K8sIoV1ObjectMeta) SetSelfLink(v string)`
+
+SetSelfLink sets SelfLink field to given value.
+
+### HasSelfLink
+
+`func (o *K8sIoV1ObjectMeta) HasSelfLink() bool`
+
+HasSelfLink returns a boolean if a field has been set.
+
+### GetUid
+
+`func (o *K8sIoV1ObjectMeta) GetUid() string`
+
+GetUid returns the Uid field if non-nil, zero value otherwise.
+
+### GetUidOk
+
+`func (o *K8sIoV1ObjectMeta) GetUidOk() (*string, bool)`
+
+GetUidOk returns a tuple with the Uid field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetUid
+
+`func (o *K8sIoV1ObjectMeta) SetUid(v string)`
+
+SetUid sets Uid field to given value.
+
+### HasUid
+
+`func (o *K8sIoV1ObjectMeta) HasUid() bool`
+
+HasUid returns a boolean if a field has been set.
 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- model_k8s_io_v1_object_meta.go
+++ model_k8s_io_v1_object_meta.go
@@ -21,8 +21,21 @@ var _ MappedNullable = &K8sIoV1ObjectMeta{}
 
 // K8sIoV1ObjectMeta struct for K8sIoV1ObjectMeta
 type K8sIoV1ObjectMeta struct {
+	Annotations *map[string]string `json:"annotations,omitempty"`
+	CreationTimestamp *string `json:"creationTimestamp,omitempty"`
+	DeletionGracePeriodSeconds *int64 `json:"deletionGracePeriodSeconds,omitempty"`
+	DeletionTimestamp *string `json:"deletionTimestamp,omitempty"`
+	Finalizers []string `json:"finalizers,omitempty"`
+	GenerateName *string `json:"generateName,omitempty"`
+	Generation *int64 `json:"generation,omitempty"`
+	Labels *map[string]string `json:"labels,omitempty"`
+	ManagedFields []K8sIoV1ManagedFieldsEntry `json:"managedFields,omitempty"`
 	Name string `json:"name"`
 	Namespace *string `json:"namespace,omitempty"`
+	OwnerReferences []K8sIoV1OwnerReference `json:"ownerReferences,omitempty"`
+	ResourceVersion *string `json:"resourceVersion,omitempty"`
+	SelfLink *string `json:"selfLink,omitempty"`
+	Uid *string `json:"uid,omitempty"`
 }
 
 type _K8sIoV1ObjectMeta K8sIoV1ObjectMeta
@@ -33,6 +46,10 @@ type _K8sIoV1ObjectMeta K8sIoV1ObjectMeta
 // will change when the set of required properties is changed
 func NewK8sIoV1ObjectMeta(name string) *K8sIoV1ObjectMeta {
 	this := K8sIoV1ObjectMeta{}
+	var creationTimestamp string = "{}"
+	this.CreationTimestamp = &creationTimestamp
+	var deletionTimestamp string = ""
+	this.DeletionTimestamp = &deletionTimestamp
 	this.Name = name
 	return &this
 }
@@ -42,7 +59,299 @@ func NewK8sIoV1ObjectMeta(name string) *K8sIoV1ObjectMeta {
 // but it doesn't guarantee that properties required by API are set
 func NewK8sIoV1ObjectMetaWithDefaults() *K8sIoV1ObjectMeta {
 	this := K8sIoV1ObjectMeta{}
+	var creationTimestamp string = "{}"
+	this.CreationTimestamp = &creationTimestamp
+	var deletionTimestamp string = ""
+	this.DeletionTimestamp = &deletionTimestamp
 	return &this
+}
+
+// GetAnnotations returns the Annotations field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetAnnotations() map[string]string {
+	if o == nil || IsNil(o.Annotations) {
+		var ret map[string]string
+		return ret
+	}
+	return *o.Annotations
+}
+
+// GetAnnotationsOk returns a tuple with the Annotations field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetAnnotationsOk() (*map[string]string, bool) {
+	if o == nil || IsNil(o.Annotations) {
+		return nil, false
+	}
+	return o.Annotations, true
+}
+
+// HasAnnotations returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasAnnotations() bool {
+	if o != nil && !IsNil(o.Annotations) {
+		return true
+	}
+
+	return false
+}
+
+// SetAnnotations gets a reference to the given map[string]string and assigns it to the Annotations field.
+func (o *K8sIoV1ObjectMeta) SetAnnotations(v map[string]string) {
+	o.Annotations = &v
+}
+
+// GetCreationTimestamp returns the CreationTimestamp field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetCreationTimestamp() string {
+	if o == nil || IsNil(o.CreationTimestamp) {
+		var ret string
+		return ret
+	}
+	return *o.CreationTimestamp
+}
+
+// GetCreationTimestampOk returns a tuple with the CreationTimestamp field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetCreationTimestampOk() (*string, bool) {
+	if o == nil || IsNil(o.CreationTimestamp) {
+		return nil, false
+	}
+	return o.CreationTimestamp, true
+}
+
+// HasCreationTimestamp returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasCreationTimestamp() bool {
+	if o != nil && !IsNil(o.CreationTimestamp) {
+		return true
+	}
+
+	return false
+}
+
+// SetCreationTimestamp gets a reference to the given string and assigns it to the CreationTimestamp field.
+func (o *K8sIoV1ObjectMeta) SetCreationTimestamp(v string) {
+	o.CreationTimestamp = &v
+}
+
+// GetDeletionGracePeriodSeconds returns the DeletionGracePeriodSeconds field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetDeletionGracePeriodSeconds() int64 {
+	if o == nil || IsNil(o.DeletionGracePeriodSeconds) {
+		var ret int64
+		return ret
+	}
+	return *o.DeletionGracePeriodSeconds
+}
+
+// GetDeletionGracePeriodSecondsOk returns a tuple with the DeletionGracePeriodSeconds field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetDeletionGracePeriodSecondsOk() (*int64, bool) {
+	if o == nil || IsNil(o.DeletionGracePeriodSeconds) {
+		return nil, false
+	}
+	return o.DeletionGracePeriodSeconds, true
+}
+
+// HasDeletionGracePeriodSeconds returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasDeletionGracePeriodSeconds() bool {
+	if o != nil && !IsNil(o.DeletionGracePeriodSeconds) {
+		return true
+	}
+
+	return false
+}
+
+// SetDeletionGracePeriodSeconds gets a reference to the given int64 and assigns it to the DeletionGracePeriodSeconds field.
+func (o *K8sIoV1ObjectMeta) SetDeletionGracePeriodSeconds(v int64) {
+	o.DeletionGracePeriodSeconds = &v
+}
+
+// GetDeletionTimestamp returns the DeletionTimestamp field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetDeletionTimestamp() string {
+	if o == nil || IsNil(o.DeletionTimestamp) {
+		var ret string
+		return ret
+	}
+	return *o.DeletionTimestamp
+}
+
+// GetDeletionTimestampOk returns a tuple with the DeletionTimestamp field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetDeletionTimestampOk() (*string, bool) {
+	if o == nil || IsNil(o.DeletionTimestamp) {
+		return nil, false
+	}
+	return o.DeletionTimestamp, true
+}
+
+// HasDeletionTimestamp returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasDeletionTimestamp() bool {
+	if o != nil && !IsNil(o.DeletionTimestamp) {
+		return true
+	}
+
+	return false
+}
+
+// SetDeletionTimestamp gets a reference to the given string and assigns it to the DeletionTimestamp field.
+func (o *K8sIoV1ObjectMeta) SetDeletionTimestamp(v string) {
+	o.DeletionTimestamp = &v
+}
+
+// GetFinalizers returns the Finalizers field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetFinalizers() []string {
+	if o == nil || IsNil(o.Finalizers) {
+		var ret []string
+		return ret
+	}
+	return o.Finalizers
+}
+
+// GetFinalizersOk returns a tuple with the Finalizers field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetFinalizersOk() ([]string, bool) {
+	if o == nil || IsNil(o.Finalizers) {
+		return nil, false
+	}
+	return o.Finalizers, true
+}
+
+// HasFinalizers returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasFinalizers() bool {
+	if o != nil && !IsNil(o.Finalizers) {
+		return true
+	}
+
+	return false
+}
+
+// SetFinalizers gets a reference to the given []string and assigns it to the Finalizers field.
+func (o *K8sIoV1ObjectMeta) SetFinalizers(v []string) {
+	o.Finalizers = v
+}
+
+// GetGenerateName returns the GenerateName field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetGenerateName() string {
+	if o == nil || IsNil(o.GenerateName) {
+		var ret string
+		return ret
+	}
+	return *o.GenerateName
+}
+
+// GetGenerateNameOk returns a tuple with the GenerateName field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetGenerateNameOk() (*string, bool) {
+	if o == nil || IsNil(o.GenerateName) {
+		return nil, false
+	}
+	return o.GenerateName, true
+}
+
+// HasGenerateName returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasGenerateName() bool {
+	if o != nil && !IsNil(o.GenerateName) {
+		return true
+	}
+
+	return false
+}
+
+// SetGenerateName gets a reference to the given string and assigns it to the GenerateName field.
+func (o *K8sIoV1ObjectMeta) SetGenerateName(v string) {
+	o.GenerateName = &v
+}
+
+// GetGeneration returns the Generation field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetGeneration() int64 {
+	if o == nil || IsNil(o.Generation) {
+		var ret int64
+		return ret
+	}
+	return *o.Generation
+}
+
+// GetGenerationOk returns a tuple with the Generation field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetGenerationOk() (*int64, bool) {
+	if o == nil || IsNil(o.Generation) {
+		return nil, false
+	}
+	return o.Generation, true
+}
+
+// HasGeneration returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasGeneration() bool {
+	if o != nil && !IsNil(o.Generation) {
+		return true
+	}
+
+	return false
+}
+
+// SetGeneration gets a reference to the given int64 and assigns it to the Generation field.
+func (o *K8sIoV1ObjectMeta) SetGeneration(v int64) {
+	o.Generation = &v
+}
+
+// GetLabels returns the Labels field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetLabels() map[string]string {
+	if o == nil || IsNil(o.Labels) {
+		var ret map[string]string
+		return ret
+	}
+	return *o.Labels
+}
+
+// GetLabelsOk returns a tuple with the Labels field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetLabelsOk() (*map[string]string, bool) {
+	if o == nil || IsNil(o.Labels) {
+		return nil, false
+	}
+	return o.Labels, true
+}
+
+// HasLabels returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasLabels() bool {
+	if o != nil && !IsNil(o.Labels) {
+		return true
+	}
+
+	return false
+}
+
+// SetLabels gets a reference to the given map[string]string and assigns it to the Labels field.
+func (o *K8sIoV1ObjectMeta) SetLabels(v map[string]string) {
+	o.Labels = &v
+}
+
+// GetManagedFields returns the ManagedFields field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetManagedFields() []K8sIoV1ManagedFieldsEntry {
+	if o == nil || IsNil(o.ManagedFields) {
+		var ret []K8sIoV1ManagedFieldsEntry
+		return ret
+	}
+	return o.ManagedFields
+}
+
+// GetManagedFieldsOk returns a tuple with the ManagedFields field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetManagedFieldsOk() ([]K8sIoV1ManagedFieldsEntry, bool) {
+	if o == nil || IsNil(o.ManagedFields) {
+		return nil, false
+	}
+	return o.ManagedFields, true
+}
+
+// HasManagedFields returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasManagedFields() bool {
+	if o != nil && !IsNil(o.ManagedFields) {
+		return true
+	}
+
+	return false
+}
+
+// SetManagedFields gets a reference to the given []K8sIoV1ManagedFieldsEntry and assigns it to the ManagedFields field.
+func (o *K8sIoV1ObjectMeta) SetManagedFields(v []K8sIoV1ManagedFieldsEntry) {
+	o.ManagedFields = v
 }
 
 // GetName returns the Name field value
@@ -101,6 +410,134 @@ func (o *K8sIoV1ObjectMeta) SetNamespace(v string) {
 	o.Namespace = &v
 }
 
+// GetOwnerReferences returns the OwnerReferences field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetOwnerReferences() []K8sIoV1OwnerReference {
+	if o == nil || IsNil(o.OwnerReferences) {
+		var ret []K8sIoV1OwnerReference
+		return ret
+	}
+	return o.OwnerReferences
+}
+
+// GetOwnerReferencesOk returns a tuple with the OwnerReferences field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetOwnerReferencesOk() ([]K8sIoV1OwnerReference, bool) {
+	if o == nil || IsNil(o.OwnerReferences) {
+		return nil, false
+	}
+	return o.OwnerReferences, true
+}
+
+// HasOwnerReferences returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasOwnerReferences() bool {
+	if o != nil && !IsNil(o.OwnerReferences) {
+		return true
+	}
+
+	return false
+}
+
+// SetOwnerReferences gets a reference to the given []K8sIoV1OwnerReference and assigns it to the OwnerReferences field.
+func (o *K8sIoV1ObjectMeta) SetOwnerReferences(v []K8sIoV1OwnerReference) {
+	o.OwnerReferences = v
+}
+
+// GetResourceVersion returns the ResourceVersion field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetResourceVersion() string {
+	if o == nil || IsNil(o.ResourceVersion) {
+		var ret string
+		return ret
+	}
+	return *o.ResourceVersion
+}
+
+// GetResourceVersionOk returns a tuple with the ResourceVersion field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetResourceVersionOk() (*string, bool) {
+	if o == nil || IsNil(o.ResourceVersion) {
+		return nil, false
+	}
+	return o.ResourceVersion, true
+}
+
+// HasResourceVersion returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasResourceVersion() bool {
+	if o != nil && !IsNil(o.ResourceVersion) {
+		return true
+	}
+
+	return false
+}
+
+// SetResourceVersion gets a reference to the given string and assigns it to the ResourceVersion field.
+func (o *K8sIoV1ObjectMeta) SetResourceVersion(v string) {
+	o.ResourceVersion = &v
+}
+
+// GetSelfLink returns the SelfLink field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetSelfLink() string {
+	if o == nil || IsNil(o.SelfLink) {
+		var ret string
+		return ret
+	}
+	return *o.SelfLink
+}
+
+// GetSelfLinkOk returns a tuple with the SelfLink field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetSelfLinkOk() (*string, bool) {
+	if o == nil || IsNil(o.SelfLink) {
+		return nil, false
+	}
+	return o.SelfLink, true
+}
+
+// HasSelfLink returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasSelfLink() bool {
+	if o != nil && !IsNil(o.SelfLink) {
+		return true
+	}
+
+	return false
+}
+
+// SetSelfLink gets a reference to the given string and assigns it to the SelfLink field.
+func (o *K8sIoV1ObjectMeta) SetSelfLink(v string) {
+	o.SelfLink = &v
+}
+
+// GetUid returns the Uid field value if set, zero value otherwise.
+func (o *K8sIoV1ObjectMeta) GetUid() string {
+	if o == nil || IsNil(o.Uid) {
+		var ret string
+		return ret
+	}
+	return *o.Uid
+}
+
+// GetUidOk returns a tuple with the Uid field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *K8sIoV1ObjectMeta) GetUidOk() (*string, bool) {
+	if o == nil || IsNil(o.Uid) {
+		return nil, false
+	}
+	return o.Uid, true
+}
+
+// HasUid returns a boolean if a field has been set.
+func (o *K8sIoV1ObjectMeta) HasUid() bool {
+	if o != nil && !IsNil(o.Uid) {
+		return true
+	}
+
+	return false
+}
+
+// SetUid gets a reference to the given string and assigns it to the Uid field.
+func (o *K8sIoV1ObjectMeta) SetUid(v string) {
+	o.Uid = &v
+}
+
 func (o K8sIoV1ObjectMeta) MarshalJSON() ([]byte, error) {
 	toSerialize,err := o.ToMap()
 	if err != nil {
@@ -111,9 +548,48 @@ func (o K8sIoV1ObjectMeta) MarshalJSON() ([]byte, error) {
 
 func (o K8sIoV1ObjectMeta) ToMap() (map[string]interface{}, error) {
 	toSerialize := map[string]interface{}{}
+	if !IsNil(o.Annotations) {
+		toSerialize["annotations"] = o.Annotations
+	}
+	if !IsNil(o.CreationTimestamp) {
+		toSerialize["creationTimestamp"] = o.CreationTimestamp
+	}
+	if !IsNil(o.DeletionGracePeriodSeconds) {
+		toSerialize["deletionGracePeriodSeconds"] = o.DeletionGracePeriodSeconds
+	}
+	if !IsNil(o.DeletionTimestamp) {
+		toSerialize["deletionTimestamp"] = o.DeletionTimestamp
+	}
+	if !IsNil(o.Finalizers) {
+		toSerialize["finalizers"] = o.Finalizers
+	}
+	if !IsNil(o.GenerateName) {
+		toSerialize["generateName"] = o.GenerateName
+	}
+	if !IsNil(o.Generation) {
+		toSerialize["generation"] = o.Generation
+	}
+	if !IsNil(o.Labels) {
+		toSerialize["labels"] = o.Labels
+	}
+	if !IsNil(o.ManagedFields) {
+		toSerialize["managedFields"] = o.ManagedFields
+	}
 	toSerialize["name"] = o.Name
 	if !IsNil(o.Namespace) {
 		toSerialize["namespace"] = o.Namespace
+	}
+	if !IsNil(o.OwnerReferences) {
+		toSerialize["ownerReferences"] = o.OwnerReferences
+	}
+	if !IsNil(o.ResourceVersion) {
+		toSerialize["resourceVersion"] = o.ResourceVersion
+	}
+	if !IsNil(o.SelfLink) {
+		toSerialize["selfLink"] = o.SelfLink
+	}
+	if !IsNil(o.Uid) {
+		toSerialize["uid"] = o.Uid
 	}
 	return toSerialize, nil
 }
